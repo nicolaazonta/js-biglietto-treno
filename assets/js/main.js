4a657a3c867e292cfd9aec6809584433a51f7b22
nicolaazonta <nicolazonta114@gmail.com>
2023-03-08 14:58:33 +0100

    define steps in pseudo-code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-alert('ocio')
+//chiedere il nome al passeggero
+//chiedere l'età al passeggero
+//chiedere il numero di chilometri che vuole percorrere
+
+//calcolare il prezzo totale del viaggio considerando che:
+    //0.21€ per ogni km 
+    //SE il passeggero è <18 lo sconto sarà del 20%
+    //SE il passeggero è >65 lo sconto sarà del 40%
+    
+//indicare il prezzo tipo 15.99€
