b423a5c75a500a78fe082042d814c133f3cbce6b
nicolaazonta <nicolazonta114@gmail.com>
2023-03-08 17:17:15 +0100

    calculate <18 and >65 prices

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,10 +7,20 @@ const passengerAge = prompt('please enter your age');
 //chiedere il numero di chilometri che vuole percorrere
 const passengerKm = Number(prompt('please enter the number of kilometers you have to travel'));
 
-
 //calcolare il prezzo totale del viaggio considerando che:
-    //0.21€ per ogni km 
+    //0.21€ per ogni km
+    let totalPrice = (passengerKm * 0.21);
+    console.log(totalPrice);
+
+
     //SE il passeggero è <18 lo sconto sarà del 20%
+    if(passengerAge < 18) { 
+        totalPrice=totalPrice - (totalPrice / 100 * 20);
+    }
+
     //SE il passeggero è >65 lo sconto sarà del 40%
-    
+    if(passengerAge > 65) { 
+        totalPrice=totalPrice - (totalPrice / 100 * 40);
+    }
+
 //indicare il prezzo tipo 15.99€
